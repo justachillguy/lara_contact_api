90d5d7d0104a44659058fd427d3212a35954ae85
justachillguy <naingyanzwe3002@gmail.com>
2023-07-18 14:01:03 +0630

    config migration

---
+++ config/info.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    "default_contact_photo" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw3EZo0G4T169q_t3NoQGXZS&ust=1689750983029000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjt0Y3bl4ADFQAAAAAdAAAAABAJ"
+
+];
